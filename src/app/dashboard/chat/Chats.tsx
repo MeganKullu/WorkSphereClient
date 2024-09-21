3404ddf2002f9a1c64f305e28d62abb8359b1b7f
MeganKullu <kullumegan@gmail.com>
2024-09-21 12:53:13 +0300

    edited chat page removed ChatDetail page

--- src/app/dashboard/chat/Chats.tsx
+++ src/app/dashboard/chat/Chats.tsx
@@ -1,50 +1,66 @@
 // here we will map over  all over the chats and and a on click function that will update the chat window
 // we will use the active buttons
 // the routes should be dynamic?
-
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-const chatId = "1";
+const dummyChats = [
+  {
+    id: "1",
+    name: "Design Chat",
+    lastMessage: "How are you today",
+    timestamp: "4m",
+  },
+  {
+    id: "2",
+    name: "Development Chat",
+    lastMessage: "The new feature is ready",
+    timestamp: "10m",
+  },
+  {
+    id: "3",
+    name: "Marketing Chat",
+    lastMessage: "The campaign was successful",
+    timestamp: "1h",
+  },
+];
 
 const Chats = () => {
+
+  const pathname = usePathname();
+
   return (
     <>
-      <Link href={`/dashboard/chat/${chatId}`} className="rounded-lg py-2 px-3 flex hover:bg-[#d5dbe7]">
-        <div className="basis-1/4 rounded-lg bg-black">{/* image goes here */}</div>
-        <div className="basis-3/4 mx-2">
-          {/* name of the use/group goes here */}
-          <div className="flex justify-between mb-1">
-            {/* here the time stamp */}
-           
-            <p className="text-base font-semibold text-black"> Design Chat</p>
-            <p className="text-gray-400"> 4m</p>
+      {/* should be scrollable too */}
+      {dummyChats.map((chat, index) => (
+        <Link
+          key={chat.id}
+          href={`/dashboard/chat/${chat.id}`}
+          className={`rounded-lg py-2 px-3 flex hover:bg-[#d5dbe7] h-16 ${
+            pathname === `/chat/${chat.id}`
+              ? "bg-[#d5dbe7]"
+              : "hover:bg-[#d5dbe7]"
+          }`}
+        >
+          <div className="basis-1/4 rounded-lg bg-black">
+            {/* image goes here */}
           </div>
-           {/* here we truncate the new message */}
-          <p>
-            How are you today
-          </p>
-        </div>
-      </Link>
-
-      <Link href={`/dashboard/chat/${chatId}`} className="rounded-lg py-2 px-3 flex hover:bg-[#d5dbe7]">
-        <div className="basis-1/4 rounded-lg bg-black">{/* image goes here */}</div>
-        <div className="basis-3/4 mx-2">
-          {/* name of the use/group goes here */}
-          <div className="flex justify-between mb-1">
-            {/* here the time stamp */}
-           
-            <p className="text-base font-semibold text-black"> Design Chat</p>
-            <p className="text-gray-400"> 4m</p>
+          <div className="basis-3/4 mx-2">
+            {/* name of the use/group goes here */}
+            <div className="flex justify-between mb-1">
+              {/* here the time stamp */}
+              <p className="text-base font-semibold text-black">{chat.name}</p>
+              <p className="text-gray-400">{chat.timestamp}</p>
+            </div>
+            {/* here we truncate the new message */}
+            <p>{chat.lastMessage}</p>
           </div>
-           {/* here we truncate the new message */}
-          <p>
-            How are you today
-          </p>
-        </div>
-      </Link>
+        </Link>
+      ))}
     </>
   );
 };
+
 export default Chats;
--- src/app/dashboard/chat/page.tsx
+++ src/app/dashboard/chat/page.tsx
@@ -1,12 +1,12 @@
 
-import ChatDetail from "../chat/[chatId]/page";
+import ChatDetail from "./[chatId]/page";
 
 export default function page({ params }: { params: string }) {
   return (
     <div className="w-full h-full flex gap-2 xl:gap-4 ">
       {/* this is gonna be a component too */}
       <div className="w-2/3 rounded-r-[30px] h-full bg-[#e8ebf6]">
-        <ChatDetail params={{ chatId: params }} />
+        {/* <ChatDetail params={{ chatId: params }} /> */}
       </div>
       <div className="w-1/3 flex flex-col gap-2 xl:gap-4">
         <div className="w-full h-1/2 bg-white rounded-[30px]"></div>

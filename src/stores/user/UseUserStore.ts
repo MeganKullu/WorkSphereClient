5f928a008a560cfc6cf8e94fa46d19ac35bd6b87
MeganKullu <kullumegan@gmail.com>
2024-09-23 15:50:46 +0300

    added type of window check for local storage

--- src/stores/user/UseUserStore.ts
+++ src/stores/user/UseUserStore.ts
@@ -8,8 +8,19 @@ interface UserState {
 const useUserStore = create<UserState>((set: any) => ({
   userId: localStorage.getItem("userId"),
   setUserId: (id: string) => {
-    localStorage.setItem("userId", id);
+    if (typeof window !== 'undefined') {
+      // Only access localStorage in the browser
+      localStorage.setItem('userId', id);
+    }
     set({ userId: id })},
 }));
+
+// Initialize the userId from localStorage if available
+if (typeof window !== 'undefined') {
+  const storedUserId = localStorage.getItem('userId');
+  if (storedUserId) {
+    useUserStore.setState({ userId: storedUserId });
+  }
+}
 
 export default useUserStore;
